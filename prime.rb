0964fc93a0a438e9369709f3b37283ef6b69cd99
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-28 03:14:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(prime_number)
+  start = 2
+  if prime_number > 1
+    range = (start..prime_number-1).to_a
+    range.none? do |num_to_test| 
+      prime_number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
